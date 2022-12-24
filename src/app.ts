4148432d53d2d669f1382f4df19c83a8d1e43508
mdsadikul <mdsadikulislam891@gmail.com>
2022-12-24 08:55:08 +0600

    Added Primitive Types

--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,13 @@ const message2 = 'Tata JavaScript';
 const number = 5378;
 console.log(message, message2, number);
 console.log(language);
+
+
+// Primitive Types(string, number, boolean)
+// unionTypes and how inferred types in typescript
+let myName :string = 'Sadikul Islam';
+
+let hisName : string; // ak line e variable declar korle se type bujte pare but onno line e variable er man set korle se type bujte pare na.
+
+hisName= "Jamal"; //
+// hisName = 32;
