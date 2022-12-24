99db8e0c018daa6fd8f1d2a8c1315a22fabe47ac
mdsadikul <mdsadikulislam891@gmail.com>
2022-12-24 18:26:45 +0600

    Explore typeAliases

--- dist/typeAliases.js
+++ dist/typeAliases.js
@@ -4,4 +4,9 @@ var herName = 'Mahbuba';
 var arr = ['Mahbuba'];
 console.log(arr);
 var students = ['Messi', 'Neymar', 847, true, 'Hello'];
-console.log(students);
+var sum = function (num1, num2, num3) {
+    if (num3 === void 0) { num3 = 0; }
+    return num1 + num2 + num3;
+};
+var total = sum(40, 45);
+console.log(total);
--- src/typeAliases.ts
+++ src/typeAliases.ts
@@ -15,4 +15,13 @@ type Students = [string, string, number, boolean, string];
 let students: Students = ['Messi', 'Neymar', 847, true, 'Hello'];
 
 
-console.log(students)
+type Add = (num1: number, num2: number, num3?: number) => number
+
+ const sum : Add = (num1: number, num2: number, num3 = 0) => {
+    return num1 + num2 + num3;
+}
+
+const total = sum(40, 45);
+
+
+console.log(total)
