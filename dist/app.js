e9ff3669770ec3a635e6d80de170c1d8ad6b0f43
mdsadikul <mdsadikulislam891@gmail.com>
2022-12-24 08:42:52 +0600

    added common.ts

---
+++ dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+var message = 'Hello TypeScript';
+var message2 = 'Tata JavaScript';
+var number = 5378;
+console.log(message, message2, number);
+console.log(language);
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+const message = 'Hello TypeScript';
+const message2 = 'Tata JavaScript';
+const number = 5378;
+console.log(message, message2, number);
+console.log(language);
